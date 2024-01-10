9a0c4c3aa22b64f58a70a7546f3c12bc9ba29024
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2024-01-10 20:17:51 -0300

    add ci/cd

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+const config = {
+    endOfLine: "lf",
+    printWidth: 80,
+    tabWidth: 4,
+    trailingComma: "es5",
+    singleQuote: false,
+    jsxSingleQuote: false,
+    semi: true,
+  };
+  
+  
+  module.exports = config;
+  
