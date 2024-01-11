e0290846c5758cdf06bef1ea921df4782e5b7901
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2024-01-10 22:29:18 -0300

    test publis

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,12 @@
 const config = {
     endOfLine: "lf",
     printWidth: 80,
-    tabWidth: 4,
+    tabWidth: 2,
     trailingComma: "es5",
     singleQuote: false,
     jsxSingleQuote: false,
     semi: true,
   };
-  
-  
+
+
   module.exports = config;
-  
