d8749a0957b10554197689b17550c3c24c4ff20b
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2024-01-11 12:29:54 -0300

    add ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+import { Options } from "prettier";
+
+const config: Options = {
+  endOfLine: "lf",
+  printWidth: 80,
+  tabWidth: 4,
+  trailingComma: "es5",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+};
+
+export = config;
