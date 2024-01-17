14fc7f68239d2bf00152c0e2e61597eb25820c4c
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2024-01-17 00:02:29 -0300

    feat: config test publish

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-
 import { Options } from "prettier";
 
 const config: Options = {
-  endOfLine: "lf",
-  printWidth: 80,
-  tabWidth: 4,
-  trailingComma: "es5",
-  singleQuote: false,
-  jsxSingleQuote: false,
-  semi: true,
+    endOfLine: "lf",
+    printWidth: 80,
+    tabWidth: 6,
+    trailingComma: "es5",
+    singleQuote: false,
+    jsxSingleQuote: false,
+    semi: true,
 };
 
 export = config;
